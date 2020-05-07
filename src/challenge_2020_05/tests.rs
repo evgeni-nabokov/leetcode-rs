0cb09428b3ba10ece1aefa2b6131669b1e093632
Evgeni Nabokov <nabokov-ef@yandex.ru>
2020-05-06 19:15:11 -0700

    Improved tests for 'Bitwise Complement'.

--- src/challenge_2020_05/tests.rs
+++ src/challenge_2020_05/tests.rs
@@ -6,24 +6,29 @@ fn num_jewels_in_stones_test() {
     assert_eq!(Solution::num_jewels_in_stones("z".to_string(), "ZZ".to_string()), 0);
 }
 
+fn get_can_construct_test_cases() -> Vec<(String, String, bool)> {
+    vec![
+        ("".to_string(), "".to_string(), true),
+        ("a".to_string(), "".to_string(), false),
+        ("".to_string(), "a".to_string(), true),
+        ("a".to_string(), "b".to_string(), false),
+        ("aa".to_string(), "ab".to_string(), false),
+        ("aa".to_string(), "aab".to_string(), true),
+    ]
+}
+
 #[test]
 fn can_construct_test() {
-    assert_eq!(Solution::can_construct("".to_string(), "".to_string()), true);
-    assert_eq!(Solution::can_construct("a".to_string(), "".to_string()), false);
-    assert_eq!(Solution::can_construct("".to_string(), "a".to_string()), true);
-    assert_eq!(Solution::can_construct("a".to_string(), "b".to_string()), false);
-    assert_eq!(Solution::can_construct("aa".to_string(), "ab".to_string()), false);
-    assert_eq!(Solution::can_construct("aa".to_string(), "aab".to_string()), true);
+    for case in get_can_construct_test_cases() {
+        assert_eq!(Solution::can_construct(case.0, case.1), case.2);
+    }
 }
 
 #[test]
 fn can_construct_v2_test() {
-    assert_eq!(Solution::can_construct_v2("".to_string(), "".to_string()), true);
-    assert_eq!(Solution::can_construct_v2("a".to_string(), "".to_string()), false);
-    assert_eq!(Solution::can_construct_v2("".to_string(), "a".to_string()), true);
-    assert_eq!(Solution::can_construct_v2("a".to_string(), "b".to_string()), false);
-    assert_eq!(Solution::can_construct_v2("aa".to_string(), "ab".to_string()), false);
-    assert_eq!(Solution::can_construct_v2("aa".to_string(), "aab".to_string()), true);
+    for case in get_can_construct_test_cases() {
+        assert_eq!(Solution::can_construct_v2(case.0, case.1), case.2);
+    }
 }
 
 fn get_bitwise_complement_test_cases() -> Vec<(i32, i32)> {

1003fa27d4bd189b312dd7611e5518e518819cc1
Evgeni Nabokov <evgeni.nabokov@gmail.com>
2020-07-02 11:34:31 -0700

    Added problem 'Binary Tree Level Order Traversal II'.

--- src/challenge_2020_07/mod.rs
+++ src/challenge_2020_07/mod.rs
@@ -1,10 +1,32 @@
 #[cfg(test)]
 mod tests;
 
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+
+use crate::common::tree_node::{TreeNode};
+
 struct Solution {}
 
 impl Solution {
     pub fn arrange_coins(n: i32) -> i32 {
         (((8f64 * n as f64 + 1f64).sqrt() - 1f64) / 2f64).floor() as i32
+    }
+
+    pub fn level_order_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        fn dfs(node: Option<Rc<RefCell<TreeNode>>>, levels: &mut Vec<Vec<i32>>, level: usize) {
+            if node.is_none() { return; }
+            if level == levels.len() {
+                levels.push(vec![]);
+            }
+            levels[level].push(RefCell::borrow(node.as_ref().unwrap()).val);
+            dfs(RefCell::borrow(node.as_ref().unwrap()).left.clone(), levels, level + 1);
+            dfs(RefCell::borrow(node.as_ref().unwrap()).right.clone(), levels, level + 1);
+        }
+
+        let mut levels: Vec<Vec<i32>> = Vec::new();
+        dfs(root, &mut levels, 0);
+        levels.into_iter().rev().collect()
     }
 }

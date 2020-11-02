6a671d68b2f2b0b91803adb48184f0846036c445
Evgeni Nabokov <evgeni.nabokov@gmail.com>
2020-11-01 21:08:27 -0800

    Added problem 'Meeting Rooms'.

--- src/challenge_2020_11/mod.rs
+++ src/challenge_2020_11/mod.rs
@@ -6,7 +6,15 @@ use crate::common::list_node::ListNode;
 struct Solution;
 
 impl Solution {
-    // 1290. Convert Binary Number in a Linked List to Integer
+    // 252. Meeting Rooms.
+    // https://leetcode.com/problems/meeting-rooms/
+    // Solution with O(N*LogN) time and O(1) space.
+    pub fn can_attend_meetings(mut intervals: Vec<Vec<i32>>) -> bool {
+        intervals.sort_unstable();
+        intervals.windows(2).all(|slice| slice[0][1] <= slice[1][0])
+    }
+
+    // 1290. Convert Binary Number in a Linked List to Integer.
     // https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
     // Solution with O(N) time and O(1) space.
     pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
--- src/challenge_2020_11/tests.rs
+++ src/challenge_2020_11/tests.rs
@@ -1,6 +1,17 @@
 use super::*;
 
 #[test]
+fn can_attend_meetings_test() {
+    let test_cases = vec![
+        (vec![vec![0, 30], vec![5, 10], vec![15, 20]], false),
+        (vec![vec![7, 10], vec![2, 4]], true),
+    ];
+    for case in test_cases {
+        assert_eq!(Solution::can_attend_meetings(case.0), case.1);
+    }
+}
+
+#[test]
 fn get_decimal_value_test() {
     let test_cases = vec![
         (vec![1, 0, 1], 5),

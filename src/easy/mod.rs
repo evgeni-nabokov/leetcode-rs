e7c0380a81d94b23fb166ac54be353ba9a4f5a68
Evgeni Nabokov <evgeni.nabokov@gmail.com>
2021-03-23 21:08:09 -0700

    Improved problem 'Two Sum'.

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -9,15 +9,17 @@ struct Solution {}
 impl Solution {
     // 1. Two Sum.
     // https://leetcode.com/problems/two-sum/
+    // Time complexity: O(N).
+    // Space complexity: O(N).
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut cache: HashMap<i32, usize> = HashMap::new();
-        cache.insert(nums[0], 0);
+        let mut map: HashMap<i32, usize> = HashMap::new();
+        map.insert(nums[0], 0);
         for i in 1..nums.len() {
             let a = target - nums[i];
-            if cache.contains_key(&a) {
-                return vec![cache[&a] as i32, i as i32]
+            if let Some(b) = map.get(&a) {
+                return vec![*b as i32, i as i32]
             } else {
-                cache.insert(nums[i], i);
+                map.insert(nums[i], i);
             }
         }
         vec![]

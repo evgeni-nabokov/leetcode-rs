eb6b0fb41438417afdbcf68ee952cedbfe70d23b
Evgeni Nabokov <evgeni.nabokov@gmail.com>
2021-11-13 17:47:35 -0800

    Minor fixes of English TrieNode.

--- src/common/english_trie.rs
+++ src/common/english_trie.rs
@@ -10,7 +10,7 @@ struct TrieNode {
 impl TrieNode {
     pub fn new(terminal: bool) -> Self {
         Self {
-            children: vec![None; 26],
+            children: vec![None; ALPHABET_SIZE],
             terminal,
         }
     }
@@ -29,8 +29,8 @@ impl Trie {
 
     pub fn insert(&mut self, word: &str) {
         let mut node = &mut self.root;
-        for b in word.bytes() {
-            let idx = Trie::get_index(b);
+        for b in word.as_bytes() {
+            let idx = Trie::get_index(*b);
             if node.children[idx].is_none() {
                 node.children[idx] = Some(TrieNode::new(false));
             }
@@ -45,8 +45,8 @@ impl Trie {
         }
 
         let mut node = &self.root;
-        for b in word.bytes() {
-            let idx = Trie::get_index(b);
+        for b in word.as_bytes() {
+            let idx = Trie::get_index(*b);
             if let Some(n) = node.children[idx].as_ref() {
                 node = n;
             } else {
@@ -62,8 +62,8 @@ impl Trie {
         }
 
         let mut node = &self.root;
-        for b in prefix.bytes() {
-            let idx = Trie::get_index(b);
+        for b in prefix.as_bytes() {
+            let idx = Trie::get_index(*b);
             if let Some(n) = node.children[idx].as_ref() {
                 node = n;
             } else {
